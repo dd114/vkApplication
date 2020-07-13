e7eba445eb91669c56e53ed71bc69b08862d7b1f
dd114 <40179423+dd114@users.noreply.github.com>
2020-07-14 02:12:45 +0500

    Не перезагружается при нажатии кнопки отправить

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import bridge from '@vkontakte/vk-bridge';
 import View from '@vkontakte/vkui/dist/components/View/View';
 import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
-import something from "./Pages/MainPages";
+import something from "./Inputs/AddData";
+import AddData from "./Inputs/AddData";
 
-function App(){
-	return (
-		something()
-	);
+function App() {
+    return (
+        <div className='wrapper'>
+            Введите данные полей
+        <AddData/>
+        </div>
+)
+
 }
 
 export default App;
--- src/Pages/MainPages.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function something() {
-    return(
-    <div>Альберт привет</div>
-    )
-}
-
-export default something;
