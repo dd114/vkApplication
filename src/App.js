65d38c2a1b09423ba5f857c1dee75db6387d22d5
dd114 <40179423+dd114@users.noreply.github.com>
2020-08-09 22:36:06 +0300

    Минорное обновлениие 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-// import React from 'react';
-// import bridge from '@vkontakte/vk-bridge';
-// import View from '@vkontakte/vkui/dist/components/View/View';
-// import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
-// import '@vkontakte/vkui/dist/vkui.css';
-
 import React from 'react';
 import '@vkontakte/vkui/dist/vkui.css';
 import TestComponents from "./TestComponents";
