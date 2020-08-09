65b415ce90d9edea1c1b653037133774e5c87e3f
dd114 <40179423+dd114@users.noreply.github.com>
2020-08-09 15:07:40 +0300

    Начинаем использовать VKUI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+// import React from 'react';
+// import bridge from '@vkontakte/vk-bridge';
+// import View from '@vkontakte/vkui/dist/components/View/View';
+// import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+// import '@vkontakte/vkui/dist/vkui.css';
+
 import React from 'react';
-import bridge from '@vkontakte/vk-bridge';
-import View from '@vkontakte/vkui/dist/components/View/View';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
-import something from "./Inputs/AddData";
-import AddData from "./Inputs/AddData";
+import TestComponents from "./TestComponents";
+
 
 function App() {
     return (
-        <div className='wrapper'>
-            Введите данные полей
-        <AddData/>
-        </div>
+        // <div className='wrapper'>
+        //     Введите данные полей
+        // </div>
+        <TestComponents/>
+
 )
 
 }
