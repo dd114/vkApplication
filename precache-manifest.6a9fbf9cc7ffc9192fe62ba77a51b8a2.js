f31e2d815333d33303b9dd67b7516b874a97e8d6
dd114 <40179423+dd114@users.noreply.github.com>
2020-08-17 17:28:40 +0400

    Updates

---
+++ precache-manifest.6a9fbf9cc7ffc9192fe62ba77a51b8a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8393a766d7c1460113816f0429ddc6",
+    "url": "/vkApplication/index.html"
+  },
+  {
+    "revision": "625f89ea6d988f9c4c8e",
+    "url": "/vkApplication/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "b0124f8c4ae79078755c",
+    "url": "/vkApplication/static/css/main.33c6a853.chunk.css"
+  },
+  {
+    "revision": "625f89ea6d988f9c4c8e",
+    "url": "/vkApplication/static/js/2.e0cd3e49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vkApplication/static/js/2.e0cd3e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0124f8c4ae79078755c",
+    "url": "/vkApplication/static/js/main.6477bd1c.chunk.js"
+  },
+  {
+    "revision": "784ee5e215db35860016",
+    "url": "/vkApplication/static/js/runtime-main.b7b64db8.js"
+  }
+]);
